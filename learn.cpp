b8683e088ec6b6b8d64956e0e8f854f78932de98
Alexode05 <alexandre.rossetti2005@gmail.com>
2024-07-15 00:48:56 +0200

    first message

--- learn.cpp
+++ learn.cpp
@@ -3,5 +3,9 @@
 int main(){
     std:: cout << "I like pizza!" << '\n';
     std:: cout << "It is very good!" << '\n';
+
+    std::string name = "alex";
+    int age = 20;
+    std::cout <<"Hello " << name << '\n' << "You are " << age << " years old" << '\n';
     return 0;
 }
