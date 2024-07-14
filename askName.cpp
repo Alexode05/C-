aa006b13d8e69b275f60bb4411bb4e9f88edea0a
Alexode05 <alexandre.rossetti2005@gmail.com>
2024-07-15 01:47:54 +0200

    finish the askName program

--- askName.cpp
+++ askName.cpp
@@ -2,7 +2,15 @@
 
 int main() {
     std::string name;
+    int age;
+
     std:: cout << "What's your name? : " << '\n';
-    std:: cin >> name;
-    std:: cout << "Hello " << name << '\n';
+    std:: getline(std::cin >> std::ws, name); // getline() is used to get a line that can contain spaces
+                                                // std::ws is used to ignore white spaces in case of the second question
+                                                // will be asked bfore the first one
+
+    std:: cout << " How old are you ?: " << '\n';
+    std:: cin >> age;
+
+    std:: cout << "Hello " << name << ", you are " << age << " years old" << '\n';
 }
