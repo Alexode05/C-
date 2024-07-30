629f8a78ae92554b7db988ac744dab8780a53956
Alexode05 <alexandre.rossetti2005@gmail.com>
2024-07-31 00:43:14 +0200

    add while and doWhile loop to ask the name

--- askName.cpp
+++ askName.cpp
@@ -5,14 +5,18 @@ int main() {
     string name;
     int age;
 
-    cout << "What's your name? : " << '\n';
-    getline(cin >> ws, name); // getline() is used to get a line that can contain spaces
-                                                // std::ws is used to ignore white spaces in case of the second question
-                                                // will be asked bfore the first one
-
-    cout << " How old are you ?: " << '\n';
-    cin >> age;
-
+    while (name.empty()){
+        cout << "What's your name? : " << '\n';
+        getline(cin >> ws, name); // getline() is used to get a line that can contain spaces
+                            // std::ws is used to ignore white spaces in case of the second question
+                            // will be asked bfore the first one
+    }
+    
+    do {
+        cout << " How old are you ?: " << '\n';
+        cin >> age;
+    } while(age == 0);
+    
     cout << "Hello " << name << ", you are " << age << " years old" << '\n';
     return 0;
 }
