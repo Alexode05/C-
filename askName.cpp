0b80a0457804da52c6c964d1881930aeff1b8778
Alexode05 <alexandre.rossetti2005@gmail.com>
2024-07-31 00:35:29 +0200

    add using namespace std;

--- askName.cpp
+++ askName.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::string name;
+    string name;
     int age;
 
-    std:: cout << "What's your name? : " << '\n';
-    std:: getline(std::cin >> std::ws, name); // getline() is used to get a line that can contain spaces
+    cout << "What's your name? : " << '\n';
+    getline(cin >> ws, name); // getline() is used to get a line that can contain spaces
                                                 // std::ws is used to ignore white spaces in case of the second question
                                                 // will be asked bfore the first one
 
-    std:: cout << " How old are you ?: " << '\n';
-    std:: cin >> age;
+    cout << " How old are you ?: " << '\n';
+    cin >> age;
 
-    std:: cout << "Hello " << name << ", you are " << age << " years old" << '\n';
+    cout << "Hello " << name << ", you are " << age << " years old" << '\n';
     return 0;
 }
